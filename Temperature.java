13a04df7af9dcdea05335a30d742758fdf8a4236
LakshmiPrasanna1593 <145839788+LakshmiPrasanna1593@users.noreply.github.com>
2024-06-23 11:08:44 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,15 @@
+package com.laddu;
+
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter temperature in your city : ");
+        float tempC = input.nextFloat();
+
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+
+    }
+}
